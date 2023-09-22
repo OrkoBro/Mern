76af1bcae11d202b8bdda844e8f9751e97b16e4b
MD. Ariful Islam (Orko) <mohammodislam45@gmail.com>
2023-09-22 18:10:57 +0600

    array delet emement

--- main.js
+++ main.js
@@ -17,8 +17,16 @@
 // let newValue = String(value);
 // console.log (typeof (newValue));
 
-let abc = ["meghla", "Rodela", "falguni", "hemonti", "Hemontika"];
+// let abc = ["meghla", "Rodela", "falguni", "hemonti", "Hemontika"];
 
-// console.log(abc[4] +" "+"ami bakruddho tumar dike cheye" +  " "+abc[5]); abc[5] number take khuje painai 
-abc.push("tumi amr gf hoba"); 
-console.log(abc[4] + "tomar jonno kichu kotha " + abc[5])
+// // console.log(abc[4] +" "+"ami bakruddho tumar dike cheye" +  " "+abc[5]); abc[5] number take khuje painai 
+// abc.push("tumi amr gf hoba"); 
+// console.log(abc[4] + "tomar jonno kichu kotha " + abc[5])
+// abc.pop()
+
+let p = [1,2,3,4,5,6,7,8,9,10];
+console.log(p);
+p.pop();
+console.log(p)
+p.shift(2);
+console.log(p);
