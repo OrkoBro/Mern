2a6d3ff129febfc0115b81a5dea4eeb5a9cb46fd
MD. Ariful Islam (Orko) <mohammodislam45@gmail.com>
2023-09-22 10:50:35 +0600

    type converson and prompt alart and conform prompt

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 // let nam =  "orko";
 // console.log(`hello ${nam} bro`)
 
-let froUndifineValue;
-console.log(froUndifineValue);
+// let froUndifineValue;
+// console.log(froUndifineValue);
+
+// let info = prompt ("hay bro tell me your name here");
+// // let info1 = prompt ("hay bro tell me your name here", "Default value");
+// console.log (info);
+
+// let cm = confirm("tell me you are the boss or not");
+// console.log(cm);
+
+
+// let value = true ;
+// console.log (typeof (value));
+// let newValue = String(value);
+// console.log (typeof (newValue));
