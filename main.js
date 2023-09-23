06745fe02deb1487a661cc0d8b5c5a7003e6a432
MD. Ariful Islam (Orko) <mohammodislam45@gmail.com>
2023-09-23 18:56:09 +0600

    unary

--- main.js
+++ main.js
@@ -1,15 +1,68 @@
-let Orko = {
-    familyName:"Mohammod Ariful Islam",
-    nickName:"Orko",
-}
-console.log(Orko);// to access the whole object
-Orko.nickName = "baca"
+// let Orko = {
+//     familyName:"Mohammod Ariful Islam",
+//     nickName:"Orko",
+// }
+// console.log(Orko);// to access the whole object
+// Orko.nickName = "baca"
 
-Orko.FatherName = "Md. Joynal Abedin";
-Orko.MotherName ="Mst. Asia Begom";
-console.log(Orko);
-delete Orko.nickName;
-console.log(Orko);
+// Orko.FatherName = "Md. Joynal Abedin";
+// Orko.MotherName ="Mst. Asia Begom";
+// console.log(Orko);
+// delete Orko.nickName;
+// console.log(Orko);
 
-console.log ("familyName" in Orko);// to chack Property exists or not 
-// if exists it will return true ortherwise return false  
+// console.log ("familyName" in Orko);// to chack Property exists or not 
+// // if exists it will return true ortherwise return false  
+
+// let nitPrice = 100.50;
+// let ShippingFee = 50.78;
+// let payableAmount = nitPrice + ShippingFee;
+// document.write(`total amount = ${payableAmount}`); 
+
+// let p = 11/5;
+// console.log(p)
+
+// let c = 11%5;
+// console.log(c);
+
+// let amar = false; //true and false er khetre kono quetes hobe na 
+// let you = +amar;
+// console.log(typeof(you));
+// console.log(you);
+
+// unary oparator with object 
+
+// let person = {
+//     name:"jone",
+//     toString:function (){
+//         return "22"; ami eita bujhi nai 
+//     }
+
+// }
+// console.log(+person);
+// ami bujhi nai 
+
+
+// let age = 24;
+// ++age;
+// age +=1;
+// age = age + 1
+// console.log(age)
+
+// let weight = 200 ;
+// weight = ++weight + 19;
+// console.log(weight);
+
+// let weight1 = 200 ;
+// weight1 = ++weight + 19;
+// console.log(weight);
+
+// let a = "20";
+// console.log(typeof(a++));
+// // console.log(a++);
+// // console.log(a)
+
+
+// let p = a++ +5 ;
+// console.log(p);
+// console.log(a);
