ff4c12b0f5fd7fe67627587a5af8fcbb6410cce2
MD. Ariful Islam (Orko) <mohammodislam45@gmail.com>
2023-09-23 10:13:51 +0600

    about object

--- main.js
+++ main.js
@@ -1,34 +1,15 @@
-// let nam =  "orko";
-// console.log(`hello ${nam} bro`)
+let Orko = {
+    familyName:"Mohammod Ariful Islam",
+    nickName:"Orko",
+}
+console.log(Orko);// to access the whole object
+Orko.nickName = "baca"
 
-// let froUndifineValue;
-// console.log(froUndifineValue);
+Orko.FatherName = "Md. Joynal Abedin";
+Orko.MotherName ="Mst. Asia Begom";
+console.log(Orko);
+delete Orko.nickName;
+console.log(Orko);
 
-// let info = prompt ("hay bro tell me your name here");
-// // let info1 = prompt ("hay bro tell me your name here", "Default value");
-// console.log (info);
-
-// let cm = confirm("tell me you are the boss or not");
-// console.log(cm);
-
-
-// let value = true ;
-// console.log (typeof (value));
-// let newValue = String(value);
-// console.log (typeof (newValue));
-
-// let abc = ["meghla", "Rodela", "falguni", "hemonti", "Hemontika"];
-
-// // console.log(abc[4] +" "+"ami bakruddho tumar dike cheye" +  " "+abc[5]); abc[5] number take khuje painai 
-// abc.push("tumi amr gf hoba"); 
-// console.log(abc[4] + "tomar jonno kichu kotha " + abc[5])
-// abc.pop()
-
-let p = [1,2,3,4,5,6,7,8,9,10];
-console.log(p);
-p.pop();
-console.log(p)
-p.shift(2);
-console.log(p);
-console.log(p.indexOf(5));
-console.log(Array.isArray(p));
+console.log ("familyName" in Orko);// to chack Property exists or not 
+// if exists it will return true ortherwise return false  
